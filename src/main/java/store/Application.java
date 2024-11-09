eb5ab12e50be46fb088094a8760dfcfe7d0bc86f
Mint <audwl03071@gmail.com>
2024-11-09 15:44:57 +0900

    fix: 실패한 부분부터 재입력받도록 수정

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -2,6 +2,7 @@ package store;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.DateTimes;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -13,33 +14,56 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Objects;
+import java.util.NoSuchElementException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
 
     public static final String INVENTORY_FILENAME = "/Users/mae/Desktop/archive/wooteco/precourse/java-convenience-store-7-Starlight258/src/main/resources/products.md";
     public static final String PROMOTION_FILENAME = "/Users/mae/Desktop/archive/wooteco/precourse/java-convenience-store-7-Starlight258/src/main/resources/promotions.md";
     public static final int FORMAT_SIZE = 12;
+    public static final String REGEX = "^\\[((\\w*\\W*)-(\\d+))\\]$";
+    public static final Pattern PATTERN = Pattern.compile(REGEX);
 
-    public static void main(String[] args) throws IOException {
-        printStartMessage();
-        Inventories inventories = addInventory();
-        showInventories(inventories);
-        Promotions promotions = addPromotion();
-        PaymentSystem paymentSystem = new PaymentSystem(inventories, promotions);
-        LocalDate now = DateTimes.now().toLocalDate();
-        convenienceStore(inventories, paymentSystem, now);
+    public static void main(String[] args) {
+        Inventories inventories = null;
+        Promotions promotions;
+        PaymentSystem paymentSystem = null;
+
+        try {
+            printStartMessage();
+            inventories = addInventory();
+            showInventories(inventories);
+            promotions = addPromotion();
+            paymentSystem = new PaymentSystem(inventories, promotions);
+        } catch (IOException ignored) {
+            System.out.println("[ERROR] 파일 형식이 잘못되었습니다.");
+        }
+
+        convenienceStore(inventories, paymentSystem);
+        Console.close();
+    }
+
+    private static void convenienceStore(final Inventories inventories, final PaymentSystem paymentSystem) {
         while (true) {
-            System.out.println(System.lineSeparator() + "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
-            String line = readLine();
-            if (line.equals("Y")) {
+            try {
+                LocalDate now = DateTimes.now().toLocalDate();
+                convenienceStore(inventories, paymentSystem, now);
+                System.out.println(System.lineSeparator() + "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
+                String line = readYOrN();
+                if (line.equals("N")) {
+                    return;
+                }
                 System.out.println();
                 printStartMessage();
                 showInventories(inventories);
-                convenienceStore(inventories, paymentSystem, now);
-                continue;
+            } catch (NoSuchElementException e) {
+                System.out.println("[ERROR] 아무것도 입력하지 않았습니다.");
+                return;
+            } catch (IllegalArgumentException | IllegalStateException exception) {
+                System.out.println(exception.getMessage());
             }
-            break;
         }
     }
 
@@ -62,16 +86,34 @@ public class Application {
 
     private static void convenienceStore(final Inventories inventories, final PaymentSystem paymentSystem,
                                          final LocalDate now) {
-        Map<String, Integer> purchasedItems = getStringIntegerMap();  // 구매할 상품의 이름
         Map<String, BigDecimal> totalNoPromotionPrice = new HashMap<>();
         Map<Product, Integer> purchasedProducts = new HashMap<>();
         Map<Product, Integer> bonusItems = new HashMap<>();
-        checkPromotion(paymentSystem, now, purchasedItems, totalNoPromotionPrice, bonusItems, purchasedProducts);
+        Map<String, Integer> purchasedItems = getPurchasedItems(inventories);
+        checkPromotion(inventories, paymentSystem, now, purchasedItems, totalNoPromotionPrice, bonusItems,
+                purchasedProducts);
         BigDecimal membershipPrice = checkMemberShip(paymentSystem, totalNoPromotionPrice);
-        showResult(inventories, purchasedProducts, bonusItems, membershipPrice);
+        showResult(purchasedProducts, bonusItems, membershipPrice);
     }
 
-    private static void showResult(final Inventories inventories, final Map<Product, Integer> purchasedProducts,
+    private static Map<String, Integer> getPurchasedItems(final Inventories inventories) {
+        System.out.println(System.lineSeparator() + "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+        while (true) {
+            try {
+                Map<String, Integer> purchasedItems = promptProductNameAndQuantity();
+                inventories.getPurchasedItems(purchasedItems, inventories);  // 구매할 상품의 이름
+                return purchasedItems;
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
+            } catch (IllegalStateException exception) {
+                System.out.println("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+            } catch (OutOfMemoryError exception) {
+                System.out.println("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+            }
+        }
+    }
+
+    private static void showResult(final Map<Product, Integer> purchasedProducts,
                                    final Map<Product, Integer> bonusItems, final BigDecimal membershipPrice) {
         Receipt receipt = new Receipt(purchasedProducts, bonusItems, membershipPrice);
         showResult(purchasedProducts, bonusItems, receipt);
@@ -80,9 +122,8 @@ public class Application {
     private static BigDecimal checkMemberShip(final PaymentSystem paymentSystem,
                                               final Map<String, BigDecimal> totalNoPromotionPrice) {
         System.out.println(System.lineSeparator() + "멤버십 할인을 받으시겠습니까? (Y/N)");
-        String line = readLine();
-        BigDecimal membershipPrice = paymentSystem.checkMembership(line, totalNoPromotionPrice);
-        return membershipPrice;
+        String line = readYOrN();
+        return paymentSystem.checkMembership(line, totalNoPromotionPrice);
     }
 
     private static void showResult(final Map<Product, Integer> purchasedProducts,
@@ -98,10 +139,7 @@ public class Application {
         BigDecimal priceToPay = receipt.getPriceToPay();
         BigDecimal totalPurchaseValue = totalPurchase.getValue();
         int blankLength = String.valueOf(totalPurchaseValue).length() - String.valueOf(priceToPay).length();
-        StringBuilder blank = new StringBuilder();
-        for (int i = 0; i < blankLength; i++) {
-            blank.append(" ");
-        }
+        String blank = " ".repeat(Math.max(0, blankLength));
         System.out.printf(
                 convert("총구매액", FORMAT_SIZE) + " \t%d \t%,.0f"
                         + System.lineSeparator(),
@@ -130,7 +168,6 @@ public class Application {
         return String.format(formatter, word);
     }
 
-
     private static void showBonus(final Map<Product, Integer> bonusItems) {
         System.out.println("=============증\t정===============");
         for (Entry<Product, Integer> entry : bonusItems.entrySet()) {
@@ -155,7 +192,8 @@ public class Application {
         }
     }
 
-    private static void checkPromotion(final PaymentSystem paymentSystem, final LocalDate now,
+    private static void checkPromotion(final Inventories inventories, final PaymentSystem paymentSystem,
+                                       final LocalDate now,
                                        final Map<String, Integer> purchasedItems,
                                        final Map<String, BigDecimal> totalNoPromotionPrice,
                                        final Map<Product, Integer> bonusItems,
@@ -163,7 +201,8 @@ public class Application {
         for (Entry<String, Integer> entry : purchasedItems.entrySet()) {
             String productName = entry.getKey();
             int quantity = entry.getValue();
-            Response response = paymentSystem.canBuy(productName, quantity, now, purchasedProducts);
+            Inventories sameProductInventories = inventories.findProducts(productName);
+            Response response = paymentSystem.canBuy(sameProductInventories, quantity, now, purchasedProducts);
             if (response.status() == ResponseStatus.BUY_WITH_NO_PROMOTION) {
                 totalNoPromotionPrice.put(productName, response.totalPrice());
                 continue;
@@ -191,12 +230,36 @@ public class Application {
         }
     }
 
-    private static String[] promptProductNameAndQuantity() {
-        System.out.println(System.lineSeparator() + "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+    public static Map<String, Integer> promptProductNameAndQuantity() {
+        Map<String, Integer> purchasedItems = new HashMap<>();
         String input = readLine();
-        input = input.replace("[", "");
-        input = input.replace("]", "");
-        return input.split(",");
+        String[] splittedText = input.split(",");
+        addPurchasedItems(purchasedItems, splittedText);
+        return purchasedItems;
+    }
+
+    private static void addPurchasedItems(final Map<String, Integer> purchasedItems, final String[] splittedText) {
+        for (String text : splittedText) {
+            Matcher matcher = PATTERN.matcher(text);
+            if (!matcher.matches()) {
+                throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+            }
+            purchasedItems.put(matcher.group(2), Converter.convertToInteger((matcher.group(3))));
+        }
+    }
+
+    private static String readYOrN() {
+        while (true) {
+            try {
+                String intent = readLine();
+                if (intent.equals("Y") || intent.equals("N")) {
+                    return intent;
+                }
+                throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
     }
 
     private static int outOfStock(final Map<Product, Integer> bonusItems, final String productName,
@@ -208,9 +271,9 @@ public class Application {
         System.out.printf(System.lineSeparator() + "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)"
                         + System.lineSeparator(),
                 productName, noPromotionQuantityOfResponse);
-        String intent = readLine();
+        String intent = readYOrN();
         Product product = response.inventory().getProduct();
-        if (Objects.equals(intent, "N")) {
+        if (intent.equals("N")) {
             purchasedProducts.put(product,
                     purchasedProducts.getOrDefault(product, 0) + quantity - noPromotionQuantityOfResponse);
             return noPromotionQuantityOfResponse;
@@ -227,27 +290,15 @@ public class Application {
             int canGetMoreQuantity = response.canGetMoreQuantity();
             System.out.printf(System.lineSeparator() + "현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"
                     + System.lineSeparator(), productName, canGetMoreQuantity);
-            String intent = readLine();
+            String intent = readYOrN();
             Product product = response.inventory().getProduct();
-            if (Objects.equals(intent, "Y")) {
+            if (intent.equals("Y")) {
                 bonusItems.put(product, bonusQuantity);
                 return canGetMoreQuantity;
             }
             bonusItems.put(product, bonusQuantity - canGetMoreQuantity);
         }
         return 0;
-    }
-
-    private static Map<String, Integer> getStringIntegerMap() {
-        String[] split = promptProductNameAndQuantity();
-        Map<String, Integer> purchasedItems = new HashMap<>();
-        for (String s : split) {
-            String[] splitInput = s.split("-");
-            String productName = splitInput[0];
-            int quantity = Converter.convertToInteger(splitInput[1]);
-            purchasedItems.put(productName, quantity);
-        }
-        return purchasedItems;
     }
 
     private static void printStartMessage() {
