85292f0529867ecad4d16bd54f7d8a88472ab952
Mint <audwl03071@gmail.com>
2024-11-10 23:22:07 +0900

    refactor: Config 클래스 생성
    - 의존성 관리 책임을 가지는 클래스를 만든다.

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -2,28 +2,14 @@ package store;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.NoSuchElementException;
+import store.config.StoreConfig;
 import store.controller.StoreController;
-import store.exception.ExceptionHandler;
-import store.support.StoreFormatter;
-import store.support.StoreSplitter;
-import store.view.InputView;
-import store.view.InteractionView;
-import store.view.OutputView;
 
 public class Application {
 
-    public static final int FORMAT_SIZE = 12;
-    public static final String DELIMITER = ",";
-
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        StoreFormatter formatter = new StoreFormatter(FORMAT_SIZE);
-        OutputView outputView = new OutputView(formatter);
-        StoreSplitter splitter = new StoreSplitter(DELIMITER);
-        ExceptionHandler exceptionHandler = new ExceptionHandler(outputView);
-        InteractionView interactionView = new InteractionView(inputView, outputView, exceptionHandler);
-        StoreController controller = new StoreController(inputView, outputView, splitter, formatter, interactionView,
-                exceptionHandler);
+        StoreConfig config = new StoreConfig();
+        StoreController controller = config.createController();
         try {
             controller.process();
         } catch (NoSuchElementException ignored) {
---
+++ src/main/java/store/config/StoreConfig.java
@@ -0,0 +1,26 @@
+package store.config;
+
+import store.controller.StoreController;
+import store.exception.ExceptionHandler;
+import store.support.StoreFormatter;
+import store.support.StoreSplitter;
+import store.view.InputView;
+import store.view.InteractionView;
+import store.view.OutputView;
+
+public class StoreConfig {
+
+    private static final int FORMAT_SIZE = 12;
+    private static final String DELIMITER = ",";
+
+    public StoreController createController() {
+        InputView inputView = new InputView();
+        StoreFormatter formatter = new StoreFormatter(FORMAT_SIZE);
+        OutputView outputView = new OutputView(formatter);
+        StoreSplitter splitter = new StoreSplitter(DELIMITER);
+        ExceptionHandler exceptionHandler = new ExceptionHandler(outputView);
+        InteractionView interactionView = new InteractionView(inputView, outputView, exceptionHandler);
+        return new StoreController(inputView, outputView, splitter, formatter, interactionView,
+                exceptionHandler);
+    }
+}
